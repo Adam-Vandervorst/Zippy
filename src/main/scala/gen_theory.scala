bb23ed6195d224b97357f708f29e7885576a4415
Adam Vandervorst <adam.vandervorst@gmail.com>
2024-05-04 21:19:11 +0200

    Add Greg's structural formulation, take 2

--- src/main/scala/gen_theory.scala
+++ src/main/scala/gen_theory.scala
@@ -1,31 +1,28 @@
 package gen
 
 trait EM[A]
-type Path[A] = A
+trait Path[A]
 
-trait A[X, E[_], V, G]:
-  case class Grounded(g: G)
-  case class Rec(x: X)
-  case class Value(v: V)
-  case class Red(x: X, ev: E[V], a: A[X,E,V,G])
-  case class Green(x: X, ev: E[V], a: A[X,E,V,G])
-  case class Pair(l: A[X,E,V,G], r: A[X,E,V,G])
+trait A[V]:
+  type X
+  type E[_]
+  type G
+  enum AL:
+    case Grounded(g: G)
+    case Rec(x: X)
+    case Value(v: V)
+    case Red(x: X, ev: E[V], a: AL)
+    case Green(x: X, ev: E[V], a: AL)
+    case Pair(l: AL, r: AL)
 
-trait Base[T]:
-  case class Empty()
-  case class Store(em: EM[T])
-  case class GBool(b: Boolean)
-  case class GInt(i: Int)
-  case class GString(i: String)
+enum Base[T]:
+  case Empty()
+  case Store(em: EM[T])
+  case GBool(b: Boolean)
+  case GInt(i: Int)
+  case GString(i: String)
 
-trait ZAM[T, V] extends A[
-  Path[EM[T]],
-  [v] =>> ZAM[T, v], // is this a good fix?
-  V,
-  Base[T]]
-
-trait RZAM[V] extends A[
-  Path[EM[RZAM[V]]],
-  RZAM,
-  V,
-  Base[RZAM[V]]]
+trait RZAM[V] extends A[V]:
+  type X = Path[EM[RZAM[V]]]
+  type E[v] = RZAM[v]
+  type G = Base[RZAM[V]]
